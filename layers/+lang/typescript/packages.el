3c7b9c17ceb5c0d5bc00dfe41710f7ca092e3f29
JAremko <w3techplayground@gmail.com>
2016-01-22 20:59:32 +0200

    initial commit

--- layers/+lang/typescript/packages.el
+++ layers/+lang/typescript/packages.el
@@ -2,21 +2,32 @@
 ;;
 ;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
-;; Author: Chris Bowdon <c.bowdon@bath.edu>
-;; URL: https://github.com/syl20bnr/spacemacs
+;; "Spacemacized" by JAremko <w3techplaygound@gmail.com> 
+;; URL: https://github.com/JAremko/spacemacs-tide-pr
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
-(setq typescript-packages '(tss))
+(setq typescript-packages '(tide))
 
-(defun typescript/init-tss ()
-  "Initialize my package"
-  (use-package tss
+(defun typescript/init-tide ()
+  "Initialize tide"
+  (use-package tide
     :defer t
     :mode ("\\.ts\\'" . typescript-mode)
-    :init (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
-            "gg" 'tss-jump-to-definition
-            "hh" 'tss-popup-help)
-    :config (tss-config-default)))
+    :init (progn
+              (tide-setup)
+              (flycheck-mode +1)
+              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+              (eldoc-mode +1)
+              (when (configuration-layer/package-usedp 'company)
+                 (company-mode-on))
+            )
+    :config (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
+            "mgg" 'tide-jump-to-definition
+            "mgb" 'tide-jump-back
+            "mhh" 'tide-documentation-at-point
+            "msr" 'tide-restart-server
+            "mrr" 'tide-reference
+            "mrn" 'tide-rename-symbol)))
